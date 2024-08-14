09d41e463244f5486fc49d332ab058e1aeaf367a
RESD\dkanf <dkanfjgwls@naver.com>
2024-08-14 23:57:35 +0900

    feat: clear Subject

--- src/_0814/BOJ_구간합구하기4.java
+++ src/_0814/BOJ_구간합구하기4.java
@@ -1,8 +1,39 @@
 package _0814;
 
-// https://www.acmicpc.net/problem/11659
-public class BOJ_구간합구하기4 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+// https://www.acmicpc.net/problem/11659
+
+public class BOJ_구간합구하기4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String[] NM = br.readLine().split(" ");
+        int N = Integer.parseInt(NM[0]);
+        int M = Integer.parseInt(NM[1]);
+        int[] numbers = new int[N];
+        int[] prefixSum = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            prefixSum[i] += numbers[i - 1] + prefixSum[i - 1];
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int cumulativeSum = prefixSum[b] - prefixSum[a - 1];
+            sb.append(cumulativeSum).append("\n");
+        }
+        System.out.println(sb);
     }
 }
--- src/_0814/BOJ_구간합구하기5.java
+++ src/_0814/BOJ_구간합구하기5.java
@@ -1,8 +1,52 @@
 package _0814;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 // https://www.acmicpc.net/problem/11660
 public class BOJ_구간합구하기5 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String[] NM = br.readLine().split(" ");
+        int N = Integer.parseInt(NM[0]);
+        int M = Integer.parseInt(NM[1]);
+        int[][] prefixSum = new int[N][N + 1];
+        int[][] number = new int[N][N];
 
+        // 1. 입력
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < N; j++) {
+                number[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 2. 행마다 구간 합 구하기
+        for (int i = 0; i < N; i++) {
+            for (int j = 1; j < N + 1; j++) {
+                prefixSum[i][j] = prefixSum[i][j - 1] + number[i][j - 1];
+            }
+        }
+
+        // 3. 누적 합 구하기, 시작 조건을 -1
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int y1 = Integer.parseInt(st.nextToken()) - 1;
+            int x1 = Integer.parseInt(st.nextToken()) - 1;
+            int y2 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int cumulative = 0;
+
+            for (int j = y1; j < y2; j++) {
+                cumulative += prefixSum[j][x2] - prefixSum[j][x1];
+            }
+            sb.append(cumulative).append("\n");
+        }
+
+        System.out.println(sb);
     }
 }
