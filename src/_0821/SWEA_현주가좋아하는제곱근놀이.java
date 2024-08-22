6c22161fcf983f8b55b3fb2361999b9ee0debe59
김현우 <dkanfjgwls@naver.com>
2024-08-22 09:21:19 +0900

    1

--- src/_0821/SWEA_현주가좋아하는제곱근놀이.java
+++ src/_0821/SWEA_현주가좋아하는제곱근놀이.java
@@ -5,28 +5,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class SWEA_현주가좋아하는제곱근놀이 {
-    static int answer;
-    static String n;
+    static long answer;
+    static long n;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
 
         for (int tc = 0; tc < t; tc++) {
-            n = br.readLine();
+            n = Long.parseLong(br.readLine());
             answer = 0;
 
-            dfs(0);
+            getN();
             System.out.printf("#%d %d\n", tc + 1, answer);
         }
     }
 
-    private static void dfs(int count) {
-        if (n.equals("2")) {
-            answer = count;
-            return;
-        }
+    private static void getN() {
+        long count = 0;
+        while (true) {
+            if (n == 2) {
+                answer = count;
+                return;
+            }
+            double sqrt = Math.sqrt(n);
+            if (sqrt % 1 == 0.0) {
+                n = (long) sqrt;
+            } else {
+                long a = (int) sqrt + 1;  // 제곱근을 정수로 바꾸고 +1
+                long b = a * a; // 제곱시키기
+                count += b - n; // 차이를 한번에 더해
+                n = b;
+                continue;
+            }
 
+            count++;
+        }
 
     }
 }
