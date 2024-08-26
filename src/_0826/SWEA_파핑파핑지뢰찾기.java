7871f694829c3b51302403b4b48b085743772ded
RESD\dkanf <dkanfjgwls@naver.com>
2024-08-27 01:28:30 +0900

    add 파핑파핑지뢰찾기

--- src/_0826/SWEA_파핑파핑지뢰찾기.java
+++ src/_0826/SWEA_파핑파핑지뢰찾기.java
@@ -7,9 +7,8 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class SWEA_파핑파핑지뢰찾기 {
-    static int N;
+    static int N, answer;
     static char[][] map;
-    static int[][] visited;
     static int[] dx = {-1, 0, 1, 1, 1, 0, -1, -1};
     static int[] dy = {-1, -1, -1, 0, 1, 1, 1, 0};
 
@@ -18,8 +17,8 @@ public class SWEA_파핑파핑지뢰찾기 {
         int T = Integer.parseInt(br.readLine());
         for (int tc = 0; tc < T; tc++) {
             N = Integer.parseInt(br.readLine());
+            answer = 0;
             map = new char[N][N];
-            visited = new int[N][N];
 
             for (int i = 0; i < N; i++) {
                 String input = br.readLine();
@@ -28,26 +27,34 @@ public class SWEA_파핑파핑지뢰찾기 {
                 }
             }
 
-            int count = 0;
+            // 1. 8칸 뚫려있는 지점부터 확인
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (map[i][j] == '.' && visited[i][j] == 0) {
+                    if (map[i][j] == '.' && check(i, j)) {
                         bfs(i, j);
-                        count++;
+                        answer++;
                     }
                 }
             }
 
-            System.out.printf("#%d %d\n", tc + 1, count);
+            // 2. 나머지 공간은 1클릭 1오픈, 그냥 더해주기
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (map[i][j] == '.') {
+                        answer++;
+                    }
+                }
+            }
 
 
+            System.out.printf("#%d %d\n", tc + 1, answer);
         }
     }
 
     private static void bfs(int i, int j) {
         Queue<int[]> queue = new ArrayDeque<>();
         queue.add(new int[]{i, j});
-        visited[i][j] = 1;
+        map[i][j] = '1'; // visited대신 map자체를 바꾸기
 
         while (!queue.isEmpty()) {
             int[] now = queue.poll();
@@ -58,13 +65,23 @@ public class SWEA_파핑파핑지뢰찾기 {
                 int nx = x + dx[k];
                 int ny = y + dy[k];
 
-                if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-                if (visited[nx][ny] == 1 || map[nx][ny] == '*') continue;
-                System.out.printf("(%d, %d) \n", nx, ny);
-                queue.add(new int[]{nx, ny});
-                visited[nx][ny] = 1;
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N || map[nx][ny] != '.') continue;
+                if (check(nx, ny)) { // 다음 8칸이 다 뚫려있는 경우만 추가
+                    queue.add(new int[]{nx, ny});
+                }
+                map[nx][ny] = '1';
             }
         }
 
+    }
+
+    private static boolean check(int x, int y) {
+        for (int i = 0; i < 8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+            if (map[nx][ny] == '*') return false;
+        }
+        return true;
     }
 }
